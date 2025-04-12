435ab9d5b8648451732622e52d52a459f6d1d9d6
ayratbai <bairat2005@gmail.com>
2025-04-12 19:03:19 +0500

    Solve a problem

--- project1/Program.cs
+++ project1/Program.cs
@@ -10,6 +10,17 @@ namespace project1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(CountBits(1234)); //1234 -> 5
+        }
+        public static int CountBits(int n)
+        {
+            int count = 0;
+            while (n > 0)
+            {
+                count += n % 2;  
+                n /= 2;          
+            }
+            return count;
         }
     }
 }
